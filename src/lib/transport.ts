5e77f4e0b142f9a8bc700492c08837108cf72d2e
Jerry Lee <lee@idoctor.co.jp>
2018-05-18 20:34:51 +0900

    add `get` method to `lib/transport.js`

--- src/lib/transport.ts
+++ src/lib/transport.ts
@@ -2,7 +2,24 @@ export class Transport {
   // constructor() {
   // }
 
-  public static text(value): any {
+  public static text(value): GoogleAppsScript.Content.TextOutput {
     return ContentService.createTextOutput(value)
   }
+
+  public static get(url: string): GoogleAppsScript.URL_Fetch.HTTPResponse {
+    return UrlFetchApp.fetch(url)
+  }
 }
+
+interface FetchOption {
+  method?: HttpMethods,
+  contentType?: string,
+  payload?: string
+  headers?: object
+  validateHttpsCertificates?: boolean
+  followRedirects?: boolean
+  muteHttpExceptions?: boolean
+  escaping?: boolean
+}
+
+enum HttpMethods { get, delete, patch, post, put }
