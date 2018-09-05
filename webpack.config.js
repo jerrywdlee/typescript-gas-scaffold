970e7e60774bcba7ce23339fb1de6b6ef6cfb5b4
Jerry Lee <lzjjerrywoodylee@gmail.com>
2018-09-05 19:01:55 +0900

    use `UglifyJsPlugin` to minify and tree shaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
+const webpack = require("webpack");
 const GasPlugin = require('gas-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const es3ifyPlugin = require('es3ify-webpack-plugin');
-const MinifyGas = require('./plugins/minify-gas');
 
 module.exports = {
   entry: './src/app.ts',
@@ -23,6 +23,13 @@ module.exports = {
     ]),
     new GasPlugin(),
     new es3ifyPlugin(),
-    new MinifyGas(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        sequences: true,  // join consecutive statemets with the “comma operator”
+        properties: false,  // optimize property access: a["foo"] → a.foo
+      },
+      mangle: false,
+      beautify: true,
+    }),
   ],
 };
